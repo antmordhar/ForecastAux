652ac7b52887c82f1be4b1b04a55c676994595f2
Antonio Morales <antmordhar@correo.ugr.es>
2020-03-22 15:14:21 +0100

    Captura de datos completada

---
+++ capturaDatos.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import pymongo
+from pymongo import MongoClient
+
+temperature = pd.read_csv("/tmp/workflow/Data/temperature.csv")
+humidity = pd.read_csv("/tmp/workflow/Data/humidity.csv")
+
+temperature= temperature[['datetime','San Francisco']]
+temperature= temperature.rename(columns={'San Francisco': 'SFTemperature'})
+
+humidity= humidity[['datetime','San Francisco']]
+humidity= humidity.rename(columns={'San Francisco': 'SFHumidity'})
+
+SF= pd.merge(temperature,humidity, on= 'datetime')
+SF= SF.dropna()
+print(SF)
+
+
+# Making a Connection with MongoClient
+client = MongoClient("mongodb://127.0.0.1:27017")
+# database
+db = client["SanFrancisco"]
+# collection
+col= db["Forecast"]
+
+SF.reset_index(inplace=True)
+data_dict=SF.to_dict("records")
+col.insert_many(data_dict)
